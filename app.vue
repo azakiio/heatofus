349d5174942df58314590e52ddadaea3999d7c55
Adham <32067614+azakiio@users.noreply.github.com>
2024-01-25 12:16:47 +0100

    try allowing cors

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 const { data, error } = await useAsyncData(
   "pdf",
   async () => {
-    const blob = await $fetch("/api/consentForm", {
+    const blob = await $fetch("https://heatofus.vercel.app/api/consentForm", {
       method: "post",
       body: { apotheke: "Hello", strasse: "hello strasse", plz: "67549" },
     });
