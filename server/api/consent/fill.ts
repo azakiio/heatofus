dba83abe53233aee3d94b3e1d1d919e25a7cada6
Adham <32067614+azakiio@users.noreply.github.com>
2024-02-02 00:17:56 +0100

    try path resolve

--- server/api/consent/fill.ts
+++ server/api/consent/fill.ts
@@ -1,45 +1,104 @@
+import fs from "fs/promises";
+import path from "path";
 import { PDFDocument } from "pdf-lib";
+import nodemailer from "nodemailer";
+import { stripIndents } from "common-tags";
 
 export default defineEventHandler(async (event) => {
-  if (event.method === "POST") {
-    const { apotheke, strasse, plz, signatureData } = await readBody(event);
-    const pdfBytes = await fetch(
-      "https://www.urbeatbro.com/api/consentForm"
-    ).then((res) => res.arrayBuffer());
+  const formData = await readFormData(event);
+  const filePath = path.resolve("./public/arzt-formular.pdf");
+  const pdfBytes = await fs.readFile(filePath);
+  const pdfDoc = await PDFDocument.load(pdfBytes);
 
-    const pdfDoc = await PDFDocument.load(pdfBytes);
+  const form = pdfDoc.getForm();
+  const page = pdfDoc.getPage(0);
+  formData.forEach((value, key) => {
+    try {
+      const field = form.getTextField(key);
+      field.setText(value.toString());
+    } catch (err) {
+      console.log(err);
+      try {
+        const radioField = form.getRadioGroup(key);
+        radioField.select(value.toString());
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+  page.drawText(new Date().toLocaleDateString("de"), {
+    x: 275,
+    y: 75,
+    size: 12,
+  });
 
-    const form = pdfDoc.getForm();
-    const page = pdfDoc.getPage(0);
+  const signatureData = formData.get("signature");
+  if (signatureData) {
+    const pdfImg = await pdfDoc.embedPng(signatureData.toString());
+    const { width, height } = pdfImg.scale(0.2);
+    page.drawImage(pdfImg, { x: 140, y: 65, width, height });
+  }
 
-    const apothekeField = form.getTextField("apothekeField");
-    const strasseField = form.getTextField("strasseField");
-    const plzField = form.getTextField("plzField");
+  const savedPDF = await pdfDoc.save();
 
-    pdfDoc.setTitle("Consent Form");
-
-    apothekeField.setText(apotheke);
-    strasseField.setText(strasse);
-    plzField.setText(plz);
-
-    page.drawText(new Date().toLocaleDateString("de"), {
-      x: 275,
-      y: 75,
-      size: 12,
+  if (formData.get("email")) {
+    const emailField = formData.get("emailField")?.toString();
+    const nameField = formData.get("nameField")?.toString();
+    const vornameField = formData.get("vornameField")?.toString();
+    const transporter = nodemailer.createTransport({
+      host: "mail.agenturserver.de",
+      port: 465,
+      auth: {
+        user: "service@elearning.bayer.de",
+        pass: "iqibizAl!235",
+      },
     });
 
-    if (signatureData) {
-      const pdfImg = await pdfDoc.embedPng(signatureData);
-      const { width, height } = pdfImg.scale(0.2);
-      page.drawImage(pdfImg, { x: 140, y: 65, width, height });
+    // TODO: Error handling
+    try {
+      await transporter.sendMail({
+        from: "service@elearning.bayer.de",
+        to: `einwilligung@jupdialog.de, ${emailField}`,
+        subject: `Consent Formular - ${vornameField} ${nameField}`,
+        text: stripIndents`Herzlichen Dank für Ihre Einwilligung.
+        Im Anhang haben wir für Sie das Dokument für Ihre Unterlagen beigefügt.
+         
+        Beste Grüße, Ihr Bayer Service-Team`,
+        attachments: [
+          {
+            filename: "Consent Formular.pdf",
+            content: Buffer.from(savedPDF),
+          },
+        ],
+      });
+    } catch (err) {
+      console.log(err);
+      return new Response(null, {
+        status: 500,
+        statusText: err as string,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      });
     }
 
-    const savedPDF = await pdfDoc.save();
-
-    return new Response(savedPDF, {
+    return new Response("Sent!", {
       headers: {
-        "Content-Type": "application/pdf",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
     });
   }
+
+  return new Response(savedPDF, {
+    headers: {
+      "Content-Type": "application/pdf",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 });
--- server/api/consent/get.ts
+++ server/api/consent/get.ts
@@ -1,143 +1,173 @@
 import { PDFDocument, rgb } from "pdf-lib";
-import { readdir, readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+import { readFile } from "node:fs/promises";
 
-export default defineEventHandler(async ({ fetch }) => {
-  // const pdf = await readFile("arzt-formular.pdf");
-
-  // const fontBytes = await fetch('/fonts/Allura-Regular.ttf').then((res) => res.arrayBuffer());
-  const dir = await readdir(".");
-
-  // const pdfDoc = await PDFDocument.load(pdf);
-  // // pdfDoc.registerFontkit(fontkit);
-  // // const signatureFont = await pdfDoc.embedFont(fontBytes);
-
-  // const form = pdfDoc.getForm();
-  // const page = pdfDoc.getPage(0);
-
-  // const genderField = form.createRadioGroup("genderField");
-  // genderField.addOptionToPage("Frau", page, {
-  //   x: 57,
-  //   y: 517,
-  //   width: 8,
-  //   height: 8,
-  //   borderColor: rgb(1, 1, 1),
+export default defineEventHandler(async ({ $fetch }) => {
+  // const pdf = await $fetch("/arzt-formular.pdf", {
+  //   baseURL: "http://localhost:3000",
   // });
+  const filePath = resolve("./public/arzt-formular.pdf");
+  const pdfBytes = await readFile(filePath);
 
-  // genderField.addOptionToPage("Herr", page, {
-  //   x: 112.5,
-  //   y: 517,
-  //   width: 8,
-  //   height: 8,
-  //   borderColor: rgb(1, 1, 1),
+  // const pdfBlob = pdf as Blob;
+  // pdfBlob.arrayBuffer();
+
+  const pdfDoc = await PDFDocument.load(pdfBytes);
+  // pdfDoc.registerFontkit(fontkit);
+  // const signatureFont = await pdfDoc.embedFont(fontBytes);
+
+  const form = pdfDoc.getForm();
+  const page = pdfDoc.getPage(0);
+
+  const genderField = form.createRadioGroup("genderField");
+  genderField.addOptionToPage("Frau", page, {
+    x: 57,
+    y: 528,
+    width: 8,
+    height: 8,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  genderField.addOptionToPage("Herr", page, {
+    x: 114.5,
+    y: 527,
+    width: 8,
+    height: 8,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  genderField.addOptionToPage("Divers", page, {
+    x: 185.5,
+    y: 528,
+    width: 8,
+    height: 8,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const titelField = form.createTextField("titelField");
+  titelField.addToPage(page, {
+    x: 235,
+    y: 528,
+    width: 177,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const vornameField = form.createTextField("vornameField");
+  vornameField.addToPage(page, {
+    x: 57,
+    y: 507,
+    width: 160,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const nameField = form.createTextField("nameField");
+  nameField.addToPage(page, {
+    x: 57,
+    y: 480,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const praxisField = form.createTextField("praxisField");
+  praxisField.addToPage(page, {
+    x: 57,
+    y: 454,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const strasseField = form.createTextField("strasseField");
+  strasseField.addToPage(page, {
+    x: 57,
+    y: 428,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const plzField = form.createTextField("plzField");
+  plzField.addToPage(page, {
+    x: 57,
+    y: 402,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const emailField = form.createTextField("emailField");
+  emailField.addToPage(page, {
+    x: 57,
+    y: 376,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const mobileField = form.createTextField("mobileField");
+  mobileField.addToPage(page, {
+    x: 57,
+    y: 350,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const telefonField = form.createTextField("telefonField");
+  telefonField.addToPage(page, {
+    x: 57,
+    y: 324,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const faxField = form.createTextField("faxField");
+  faxField.addToPage(page, {
+    x: 57,
+    y: 298,
+    width: 355,
+    height: 13,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  const communicationField = form.createRadioGroup("communicationField");
+  communicationField.addOptionToPage("email", page, {
+    x: 256,
+    y: 264,
+    width: 8,
+    height: 8,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  communicationField.addOptionToPage("fax", page, {
+    x: 327,
+    y: 263,
+    width: 8,
+    height: 8,
+    borderColor: rgb(1, 1, 1),
+  });
+
+  // const signatureField = form.createTextField('signatureField');
+  // signatureField.addToPage(page, {
+  // 	x: 100,
+  // 	y: 70,
+  // 	width: 150,
+  // 	height: 25,
+  // 	borderColor: rgb(1, 1, 1),
   // });
+  // signatureField.setFontSize(16);
+  // signatureField.setAlignment(TextAlignment.Center);
+  // signatureField.updateAppearances(signatureFont);
 
-  // const titelField = form.createTextField("titelField");
-  // titelField.addToPage(page, {
-  //   x: 163,
-  //   y: 516,
-  //   width: 249,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
+  pdfDoc.setTitle("Arzt Einwilligung-Formular");
 
-  // const vornameField = form.createTextField("vornameField");
-  // vornameField.addToPage(page, {
-  //   x: 57,
-  //   y: 490,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
+  const savedPDF = await pdfDoc.save();
 
-  // const nameField = form.createTextField("nameField");
-  // nameField.addToPage(page, {
-  //   x: 57,
-  //   y: 464,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // const apothekeField = form.createTextField("apothekeField");
-  // apothekeField.addToPage(page, {
-  //   x: 57,
-  //   y: 438,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // const strasseField = form.createTextField("strasseField");
-  // strasseField.addToPage(page, {
-  //   x: 57,
-  //   y: 412,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // const plzField = form.createTextField("plzField");
-  // plzField.addToPage(page, {
-  //   x: 57,
-  //   y: 386,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // const emailField = form.createTextField("emailField");
-  // emailField.addToPage(page, {
-  //   x: 57,
-  //   y: 360,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // const mobileField = form.createTextField("mobileField");
-  // mobileField.addToPage(page, {
-  //   x: 57,
-  //   y: 334,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // const telefonField = form.createTextField("telefonField");
-  // telefonField.addToPage(page, {
-  //   x: 57,
-  //   y: 308,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // const faxField = form.createTextField("faxField");
-  // faxField.addToPage(page, {
-  //   x: 57,
-  //   y: 282,
-  //   width: 355,
-  //   height: 13,
-  //   borderColor: rgb(1, 1, 1),
-  // });
-
-  // // const signatureField = form.createTextField('signatureField');
-  // // signatureField.addToPage(page, {
-  // // 	x: 100,
-  // // 	y: 70,
-  // // 	width: 150,
-  // // 	height: 25,
-  // // 	borderColor: rgb(1, 1, 1),
-  // // });
-  // // signatureField.setFontSize(16);
-  // // signatureField.setAlignment(TextAlignment.Center);
-  // // signatureField.updateAppearances(signatureFont);
-
-  // pdfDoc.setTitle("Consent Form");
-
-  // const savedPDF = await pdfDoc.save();
-
-  return new Response(dir.join("/n"), {
+  return new Response(savedPDF, {
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
