5ec3bb921a2023796b719a2f0130bdd8840b6361
Adham <32067614+azakiio@users.noreply.github.com>
2024-04-04 19:34:02 +0200

    update middleware

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,4 +1,5 @@
 export default defineEventHandler((event) => {
+  console.log("cors middleware");
   setResponseHeaders(event, {
     "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "Access-Control-Allow-Origin": "*",
@@ -7,8 +8,8 @@ export default defineEventHandler((event) => {
     "Access-Control-Expose-Headers": "*",
   });
   if (event.method === "OPTIONS") {
-    event.node.res.statusCode = 204;
-    event.node.res.statusMessage = "No Content.";
+    console.log("preflight");
+    setResponseStatus(event, 204, "No Content.");
     return "OK";
   }
 });
