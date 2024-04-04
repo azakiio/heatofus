a9e4de9fe53a55944af0f617ee773efb159a72d5
Adham <32067614+azakiio@users.noreply.github.com>
2024-04-04 19:09:04 +0200

    add send api

---
+++ server/api/bestellung/send.ts
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+export default defineEventHandler(async (event) => {
+  const { email, output } = await readBody(event);
+  if (email) {
+    const transporter = nodemailer.createTransport({
+      host: "mail.agenturserver.de",
+      port: 465,
+      auth: {
+        user: "service@elearning.bayer.de",
+        pass: "iqibizAl!235",
+      },
+    });
+
+    // TODO: Error handling
+    try {
+      await transporter.sendMail({
+        from: "service@elearning.bayer.de",
+        to: `${email}`, // , einwilligung@jupdialog.de, ISR_Arzt_Consent@bayer.com`,
+        subject: `Allgemeinmedizin Bestellung`,
+        text: output,
+      });
+    } catch (err) {
+      console.log(err);
+      return new Response(null, {
+        status: 500,
+        statusText: err as string,
+      });
+    }
+  }
+
+  return new Response("Sent!");
+});
