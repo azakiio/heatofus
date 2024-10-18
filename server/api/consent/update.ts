f3986ce926c941d7d80aa7f41486cd213b520637
Adham <32067614+azakiio@users.noreply.github.com>
2024-10-18 13:49:39 -0400

    add emails to bcc

--- server/api/consent/update.ts
+++ server/api/consent/update.ts
@@ -87,16 +87,18 @@ export default defineEventHandler(async (event) => {
       },
     });
 
-    let to = `${emailField}, thomas.mantei@janus-wa.de`;
+
+    let bcc = `thomas.mantei@janus-wa.de`
 
     if (tagData?.toString() === "CHAL") {
-      to = `${emailField}, einwilligung@jupdialog.de, ISR_Arzt_Consent@bayer.com`;
+      bcc = `einwilligung@jupdialog.de, ISR_Arzt_Consent@bayer.com`;
     }
     // TODO: Error handling
     try {
       await transporter.sendMail({
         from: "service@elearning.bayer.de",
-        to: to,
+        to: emailField,
+        bcc,
         subject: `Consent Formular - ${vornameField} ${nameField}`,
         text: stripIndents`Herzlichen Dank, dass Sie uns einen Consent für die digitale Kommunikation mit Ihnen erteilt haben.
 
